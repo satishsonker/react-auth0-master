bc325ba50698d4c6edb5ba8df3da3ff02ddc7972
Satish Sonkar <satish.sonkar@nagarro.com>
2021-10-27 20:38:08 +0530

    update

--- react-auth0-master/src/components/CustomView/Unauthorized.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Unauthorized() {
-    return (
-        <div className="text-center">
-            <h4>Unauthorised or not have permission to access this resource</h4>
-        </div>
-    )
-}
